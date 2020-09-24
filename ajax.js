5f3313c4852e4053963c41f047626386ac504e1d
isaackumi <isaac.kumi@ashesi.edu.gh>
2020-09-24 22:12:06 +0000

    added payment controller[WIP]

--- ajax.js
+++ ajax.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         var id = $("#id"). val();
         var key = $("#key"). val();
         const endpoint = "https://apps.ashesi.edu.gh/e-com/";
-        alert(amount);
+        // alert(amount);
 
             const user  = {
                 'amt':amount,
@@ -28,41 +28,6 @@ $(document).ready(function(){
         });
 
     });
-
-    // document.getElementById("btn").onclick = function() {
-    //
-    //     const endpoint = "https://apps.ashesi.edu.gh/e-com/";
-    //     var amount = $("#amount"). val();
-    //     var id = $("#id"). val();
-    //     var key = $("#key"). val();
-    //
-    //     // alert(amount);
-    //     // alert(id);
-    //     // alert(key);
-    //
-    //
-    //
-    //
-    //     const user  = {
-    //         'amt':amount,
-    //         'userid':id,
-    //         'secretkey': key
-    //
-    //     }
-    //     axios({
-    //         method:"POST",
-    //         url: endpoint,
-    //         data: {
-    //             user
-    //         }
-    //     }).then(data => alert(data))
-    //         .catch(err => alert(err))
-    //
-    //
-    //
-    //
-    //
-    // };
 
 
 
